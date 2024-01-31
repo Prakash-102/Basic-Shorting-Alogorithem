c5c75b77634a8fe10c0c1564dfafabe0be58e4e9
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-31 17:28:34 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,35 @@
+
+//Bubble Short ?:- 
+import java.util.*;
+
+public class Sort {
+
+    public static void bubblesort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        bubblesort(arr);
+        printArr(arr);
+    }
+}
+/*
+ * Output:-
+ * 1 2 3 4 5
+ */
